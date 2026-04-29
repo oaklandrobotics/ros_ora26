5777431147b1f8054736d0f6a286b4a9d2d4f4e1
dmocnik <dmocnik@oakland.edu>
2026-04-28 22:02:35 -0400

    add docker compose

--- zed-jetson/run.sh
+++ zed-jetson/run.sh
@@ -6,8 +6,6 @@ docker run \
   -e NVIDIA_DRIVER_CAPABILITIES="all" \
   -v /tmp:/tmp \
   -v /dev:/dev \
-  -v /var/nvidia/nvcam/settings/:/var/nvidia/nvcam/settings/ \
-  -v /etc/systemd/system/zed_x_daemon.service:/etc/systemd/system/zed_x_daemon.service \
   -v /usr/local/zed/resources/:/usr/local/zed/resources/ \
   -v /usr/local/zed/settings/:/usr/local/zed/settings/ \
   dmocnik/ora_zed:latest
